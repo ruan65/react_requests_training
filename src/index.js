86855932a5e98f5b7079e382e0643ec217ec9746
a <ruan65@gmail.com>
2017-12-30 00:02:03 +0300

    add interseptors to requests and responses

--- src/index.js
+++ src/index.js
@@ -4,5 +4,29 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import Axios from 'axios'
+
+Axios.interceptors.request.use(requestConfig => {
+  console.log(requestConfig)
+  
+  //Edit requestConfig
+  
+  return requestConfig
+}, error => {
+  console.log(error)
+  return Promise.reject(error)
+})
+
+Axios.interceptors.response.use(requestConfig => {
+  console.log(requestConfig)
+  
+  //Edit requestConfig
+  
+  return requestConfig
+}, error => {
+  console.log(error)
+  return Promise.reject(error)
+})
+
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
