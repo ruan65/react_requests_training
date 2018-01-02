38c2db71a27c29bb85914de189151c702345c9f1
a <ruan65@gmail.com>
2018-01-02 13:38:45 +0300

    reduce example

--- src/containers/Scripts/Script.js
+++ src/containers/Scripts/Script.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react'
 import data from './data.txt'
 
-
-function reduceFile (input) {
-  console.log(input)
+function reduceFile( input ) {
+  let output = input
+    .trim()
+    .split('\n')
+    .map(line => line.split('\t'))
+    .reduce(( customers, line ) => {
+      
+      // if (!customers[line[0]]) {
+      //   customers[line[0]] = []
+      // }
+      
+      customers[line[0]] = customers[line[0]] || []
+      customers[line[0]].push({ name: line[1], price: line[2], count: line[3] })
+      return customers
+    }, {})
+  
+  return output
 }
 
 class Script extends Component {
   
   runScriptHandler() {
     
-    fetch(data).then(d => {
-      
-      d.text().then(t => reduceFile(t))
-  
-    })
+    fetch(data)
+      .then(d => {
+        
+        d.text().then(t => {
+          
+          console.log('output', JSON.stringify(reduceFile(t), null, 2))
+        })
+        
+      })
   }
   
   render() {
