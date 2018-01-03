dfe7cd9fbc204e707aa1fa3c25e519f05be188f7
a <ruan65@gmail.com>
2018-01-03 17:01:33 +0300

    redirecting by pushing or replacing props history

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
 import Script from '../Scripts/Script'
@@ -38,6 +38,7 @@ class Blog extends Component {
           <Route path='/new-post' exact component={NewPost}/>
           <Route path='/scripts' exact component={Script}/>
           <Route path='/posts' component={Posts}/>
+          <Redirect from='/' to='/posts' />
         </Switch>
       </div>
     );
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,51 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Axios from 'axios'
+import { Redirect } from 'react-router-dom'
 import './NewPost.css';
 
 class NewPost extends Component {
-    state = {
-        title: '',
-        content: '',
-        author: 'Andreyka'
+  state = {
+    title: '',
+    content: '',
+    author: 'Andreyka',
+    submitted: false
+  }
+  
+  postDataHandler = () => {
+    
+    const post = {
+      title: this.state.title,
+      content: this.state.content,
+      author: this.state.author
     }
     
-    postDataHandler = () => {
+    console.log('Post data handler: ' + JSON.stringify(post))
     
-      const post = {
-        title: this.state.title,
-        content: this.state.content,
-        author: this.state.author
-      }
-      
-      console.log('Post data handler: ' + JSON.stringify(post))
-      
-      Axios.post('/posts', post)
-        .then(response => {
-          console.log(response)
-        })
+    Axios.post('/posts', post)
+      .then(response => {
+        console.log(response)
+        // this.setState({submitted: true})
+        // this.props.history.push('/posts')
+        this.props.history.replace('/posts')
+      })
+  }
+  
+  componentDidMount() {
+    console.log(this.props)
+  }
+  
+  render() {
+    if (this.state.submitted) {
+      return <Redirect to='/posts'/>
     }
-    
-    componentDidMount() {
-      console.log(this.props)
-    }
-
-    render () {
-        return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Andrew">Andrew</option>
-                    <option value="Marina">Marina</option>
-                </select>
-                <button onClick={this.postDataHandler}>Add Post Now</button>
-            </div>
-        );
-    }
+    return (
+      <div className="NewPost">
+        <h1>Add a Post</h1>
+        <label>Title</label>
+        <input type="text" value={this.state.title}
+               onChange={( event ) => this.setState({ title: event.target.value })}/>
+        <label>Content</label>
+        <textarea rows="4" value={this.state.content}
+                  onChange={( event ) => this.setState({ content: event.target.value })}/>
+        <label>Author</label>
+        <select value={this.state.author} onChange={( event ) => this.setState({ author: event.target.value })}>
+          <option value="Andrew">Andrew</option>
+          <option value="Marina">Marina</option>
+        </select>
+        <button onClick={this.postDataHandler}>Add Post Now</button>
+      </div>
+    );
+  }
 }
 
 export default NewPost;
